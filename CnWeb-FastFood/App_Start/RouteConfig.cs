ef8f8fcb23a1bb42c5e2f0d77c9aa3fcb1a2f932
= <=>
2020-06-15 16:58:39 +0700

    hieu 1

--- CnWeb-FastFood/App_Start/RouteConfig.cs
+++ CnWeb-FastFood/App_Start/RouteConfig.cs
@@ -16,8 +16,9 @@ namespace CnWeb_FastFood
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Dashboard", action = "Index", id = UrlParameter.Optional }        
+            ).DataTokens.Add("area", "Admin");
+   
         }
     }
 }
